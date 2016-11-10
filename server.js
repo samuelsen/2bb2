2c54c227779ec0fd344c7b7c2e38e664a2332924
Bjørn Moholt <bjornmoh@login.ifi.uio.no>
2016-11-10 16:55:37 +0100

    changed ip binding from localhost to 0.0.0.0

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var boundIp = '0.0.0.0';
+
 const manifest = require('./src/manifest.js');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, boundIp, function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://' + boundIp + ':3000/');
 });
