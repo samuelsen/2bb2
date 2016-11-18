00f87409d9cf23f8f66dbad4a569fed7e0d588cd
Eirik Isene <iseneirik@gmail.com>
2016-11-18 16:44:01 +0100

    finished with major linting fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // src/index.jsx
 
-import React, { component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import App from './App';
 import Create from './components/Create';
@@ -14,7 +14,7 @@ import Test from './components/Test';
 import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
   applyMiddleware(sagaMiddleware)
@@ -22,31 +22,29 @@ const store = createStore(
 sagaMiddleware.run(rootSaga);
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 // TEST DATA (UNCOMMENT yield delay(1000); IN SAGAS.JS)
-store.dispatch({type: 'FETCH_NAMESPACES'});
-store.dispatch({type: 'FETCH_KEYS', namespace: "METADATASTORE"});
-store.dispatch({type: 'FETCH_KEYS', namespace: "social-media-video"});
-store.dispatch({type: 'FETCH_KEYS', namespace: "newspace"});
-//store.dispatch({type: 'FETCH_KEYS', namespace: "LOLZ"});
-//store.dispatch({type: 'FETCH_DATA', namespace: "METADATASTORE", key: "Version_1"});
-//store.dispatch({type: 'FETCH_DATA', namespace: "social-media-video", key: "hjcF14oVjo4"});
-//store.dispatch({type: 'CREATE_DATA', namespace: "LOLZ", key: "Version_X", body: "{ \"value\": \"TEST\"}"});
-//store.dispatch({type: 'CREATE_DATA', namespace: "LOLZ", key: "Version_Y", body: "{ \"value\": \"TEST\"}"});
-//store.dispatch({type: 'FETCH_DATA', namespace: "METADATASTORE", key: "Version_Y"});
-//store.dispatch({type: 'DELETE_KEY', namespace: "LOLZ", key:"Version_X"});
-//store.dispatch({type: 'DELETE_NAMESPACE', namespace: "LOLZ"});
+store.dispatch({ type: 'FETCH_NAMESPACES' });
+store.dispatch({ type: 'FETCH_KEYS', namespace: 'METADATASTORE' });
+store.dispatch({ type: 'FETCH_KEYS', namespace: 'social-media-video' });
+store.dispatch({ type: 'FETCH_KEYS', namespace: 'newspace' });
+// store.dispatch({type: 'FETCH_KEYS', namespace: "LOLZ"});
+// store.dispatch({type: 'FETCH_DATA', namespace: "METADATASTORE", key: "Version_1"});
+// store.dispatch({type: 'FETCH_DATA', namespace: "social-media-video", key: "hjcF14oVjo4"});
+// store.dispatch({type: 'FETCH_DATA', namespace: "METADATASTORE", key: "Version_Y"});
+// store.dispatch({type: 'DELETE_KEY', namespace: "LOLZ", key:"Version_X"});
+// store.dispatch({type: 'DELETE_NAMESPACE', namespace: "LOLZ"});
 
 render(
-    <Provider store={store}>
+  <Provider store={store}>
     { /* Tell the Router to use our enhanced history */ }
-        <Router history={history}>
-            <Route path="/" component={App}></Route>
-            <Route path="/test" component={Test}></Route>
-            <Route path="/viewData/:namespace/:key" component={Test}></Route>
-            <Route path="/create" component={Create}></Route>
-        </Router>
-    </Provider>,
-    document.getElementById('app')
-)
+    <Router history={history}>
+      <Route path="/" component={App} />
+      <Route path="/test" component={Test} />
+      <Route path="/viewData/:namespace/:key" component={Test} />
+      <Route path="/create" component={Create} />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
--- src/reducers/collapsibleReducer.js
+++ src/reducers/collapsibleReducer.js
@@ -1,70 +1,37 @@
-// src/reducers/todoReducer.js
+import { List, Map } from 'immutable';
 
-import { List, Map } from 'immutable';
-
-const init = Map({'entries': List([{
-  namespace: "DEFAULT NAMESPACE",
-  ids: ["DUMMY 1", "DUMMY 2"]
-}])});
-
-export default function collapsibleReducer(state=init, action) {
+export default function collapsibleReducer(state = new Map(), action) {
   switch (action.type) {
     case 'NAMESPACES_FETCHED':
-      return state.set('entries', List(action.entries));
+      return state.set('entries', new List(action.entries));
     case 'KEYS_FETCHED':
       return state.update('entries', entries => entries.update(
         entries.findIndex(item => item.namespace === action.namespace),
-        entry => { return { namespace: entry.namespace, ids: action.keys } }
+        entry => ({ namespace: entry.namespace, ids: action.keys })
       ));
     case 'DATA_FETCHED':
-      console.log(action.data);
       /* TODO */
       return state;
     case 'DATA_CREATED':
-      console.log(action.data);
       /* TODO */
       return state;
     case 'DATA_MODIFIED':
-      console.log(action.data);
       /* TODO */
       return state;
     case 'NAMESPACE_DELETED':
-      console.log(action.data);
       /* TODO */
       return state;
     case 'KEY_DELETED':
-      console.log(action.data);
       /* TODO */
       return state;
     case 'FETCH_FAILED':
     case 'CREATE_FAILED':
     case 'DELETE_FAILED':
     case 'MODIFY_FAILED':
-      console.log(action.error.responseText);
+      // console.log(action.error.responseText);
+      /* TODO */
+      return state;
     default:
       return state;
   }
 }
-
-// src/reducers/todoReducer.js
-// EXAMPLE CODE
-//import { List, Map } from 'immutable';
-//
-//const init = Map({'todos': List([])});
-//
-//export default function todoReducer(state=init, action) {
-//  switch (action.type) {
-//    case 'ADD_TODO':
-//      return state.update('todos', todos => todos.push(Map(action.payload)));
-//    case 'TOGGLE_TODO':
-//      return state.update('todos', todos => todos.map(t => {
-//        if(t.get('id') === action.payload) {
-//          return t.update('isDone', isDone => !isDone);
-//        } else {
-//          return t;
-//        }
-//      }));
-//    default:
-//        return state;
-//  }
-//}
