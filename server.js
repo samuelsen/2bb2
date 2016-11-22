3da149d8bc16bcb3b2b2f8d4af1552fd9482a786
Simen Samuelsen <simendsa@ulrik.uio.no>
2016-11-22 09:02:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, boundIp, function (err, result) {
+}).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Listening at http://' + boundIp + ':3000/');
+  console.log('Listening at http://' + 'localhost' + ':3000/');
 });
