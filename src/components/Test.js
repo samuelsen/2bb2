e2d0a8cb2b1a5a2c1fd8a251282a680f0e668156
Simen Samuelsen <simen.samuelsen@gmail.com>
2016-11-23 16:13:09 +0100

    updates

---
+++ src/components/Test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class Test extends Component {
+  render(){
+    
+    const fs = require('fs');
+        
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+  }
+}
