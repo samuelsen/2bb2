301dd7b235d5c7712beddaab06bf7b2640f8b695
Simen Samuelsen <simen.samuelsen@gmail.com>
2016-11-26 13:31:48 +0100

    some testing

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
-var boundIp = '0.0.0.0';
+const boundIp = '0.0.0.0';
 
 const manifest = require('./src/manifest.js');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
